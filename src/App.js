c3c7d3a727d38aa7643666a94c71db56dac1fd35
MD RAIHANUL KABIR <rkbokul@gmail.com>
2022-05-20 13:14:24 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import Home from './Pages/Shared/Home';
+import Navbar from '../src/Pages/Shared/Navbar'
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
