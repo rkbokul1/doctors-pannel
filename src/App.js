7ad3463018aa7e52e347c6b71571664988d0f940
MD RAIHANUL KABIR <rkbokul@gmail.com>
2022-05-20 13:57:45 +0600

    all route setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Navbar from '../src/Pages/Shared/Navbar'
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Review from './Pages/Review/Review';
+import Contact from './Pages/Contact/Contact';
+import Login from './Pages/Login/Login';
+import Appointment from './Pages/Contact/Appointment/Appointment';
+import Error from './Pages/Error/Error';
 
 function App() {
   return (
@@ -10,7 +15,13 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/appointment' element={<Appointment />}></Route>
+        <Route path='/review' element={<Review />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/*' element={<Error/>}></Route>
       </Routes>
     </div>
   );
